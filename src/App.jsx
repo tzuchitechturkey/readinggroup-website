e08cd2cfac3822d4d7e5fa1316b30c78bc087235
muhammedakil <muhammed@tzuchitech.com>
2025-09-22 08:11:32 +0300

    fix: Adjust package dependencies order and clean up Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import NonAuthLayout from "@/components/ForPages/Auth/NonAuthLayout/NonAuthLayout";
-
-<<<<<<< HEAD
-import NonAuthLayout from "@/components/ForPages/Auth/NonAuthLayout/NonAuthLayout";
 import Navbar from "@/components/Global/Navbar/Navbar";
 
-=======
->>>>>>> main
 import { userRoutes, authRoutes } from "./routes/allRoutes";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className=" border" style={{ minHeight: "100vh" }}>
-      <Navbar />
-=======
     <div className="min-h-[100vh]">
->>>>>>> main
+      <Navbar />
       <Routes>
         {authRoutes.map((route, idx) => (
           <Route
--- src/components/Global/Navbar/Navbar.jsx
+++ src/components/Global/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <div>
       <Usernavbar />
-      <AdminNavbar />
+      {/* <AdminNavbar /> */}
     </div>
   );
 }
