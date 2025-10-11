2d292e2a825500285e4c0b8aef5d11adc6f0070b
Ayman Khalil <ayman.khalil33@gmail.com>
2025-10-11 13:33:33 +0300

    fix urls

--- backend/readinggroup_backend/urls.py
+++ backend/readinggroup_backend/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="home"),
     path(
         "api/docs/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
