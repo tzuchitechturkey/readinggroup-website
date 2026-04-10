aaa37ebcf54d057b0578284af8ddc3319f00608e
Ayman Khalil <ayman.khalil33@gmail.com>
2026-04-10 10:56:28 +0300

    add cors allowed

--- backend/readinggroup_backend/settings.py
+++ backend/readinggroup_backend/settings.py
@@ -62,9 +62,9 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
